e51ce345260a7bd297a12af90db2d990f720ec6b
samuel jacobsen1606 <samuelpj1606@gmail.com>
2023-12-05 21:17:26 -0300

    add RateLimit

--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,20 @@
 const express = require('express');
-const cors = require('cors'); 
+const cors = require('cors');
+const rateLimit = require('express-rate-limit');
 
 const app = express();
 
-app.use(cors()); 
-
+app.use(cors());
 app.use(express.json());
+
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutos
+  max: 100, // máximo de 100 solicitações
+  message: 'Limite de solicitações excedido, por favor, tente novamente mais tarde.',
+});
+
+app.use(limiter);
 
 const UserRoutes = require('./routes/UserRoutes');
 const MovieRoutes = require('./routes/MovieRoutes');
@@ -14,5 +23,5 @@ app.use('/users', UserRoutes);
 app.use('/movies', MovieRoutes);
 
 app.listen(8080, () => {
-    console.log('Servidor está rodando na porta 8080');
+  console.log('Servidor está rodando na porta 8080');
 });
